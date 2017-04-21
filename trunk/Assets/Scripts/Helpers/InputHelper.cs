9d03e17ea491409007acbfcf4612119e18e45dc0
Adam <nosal.adam11@gmail.com>
2017-04-21 23:25:56 +0200

    adding gamepad Lib to main Project

---
+++ trunk/Assets/Scripts/Helpers/InputHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helpers
+{
+    public class InputHelper
+    {
+
+
+        public static string MOVE_JOYSTICK_HORIZONTAL = "HorizontalMovement";
+
+        public static string MOVE_JOYSTICK_VERTICAL = "VerticalMovement";
+
+        public static string ROTATION_JOYSTICK_HORIZONTAL = "HorizontalRotation";
+
+        public static string ROTATION_JOYSTICK_VERTICAL = "VerticalRotation";
+    }
+}
---
+++ trunk/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using Helpers;
+
+public class PlayerController : MonoBehaviour
+{
+
+ 
+    [Header("Controllers")]
+    [SerializeField]
+    private Rigidbody2D mRigidbody2d;
+    
+    private Transform mTransform;
+    
+
+    [Header("Movement")]
+    [SerializeField]
+    [Range(5.0f,15.0f)]
+    private float speed = 7.5f;
+
+
+
+
+
+    [SerializeField] private Vector2 lookVector;
+    [SerializeField] private Vector3 destinationRotation;
+    [SerializeField] private int healthPoints = 100;
+
+    private float movement;
+
+
+
+    void Awake()
+    {
+        InitVariables();
+            
+    }
+    
+    void Update()
+    {
+
+       Vector2 lVector = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.RightStick, GamepadInput.GamePad.Index.One);
+        if(Mathf.Abs(lVector.x)>0.2f || Mathf.Abs(lVector.y) > 0.2f)
+        {
+            lookVector = lVector;
+        }
+        
+        if (Mathf.Abs(lookVector.x) > 0.25f)
+        {
+            Vector3 diff = lookVector;// - transform.position;
+            diff.Normalize();
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+        }
+
+        movement = GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.LeftStick, GamepadInput.GamePad.Index.One).y;
+        int forward = movement > 0 ? 1 : -1;
+        if (Mathf.Abs( movement) > 0.25f)
+        {
+            mRigidbody2d.velocity = speed * forward* lookVector;
+
+        }
+        else
+        {
+            DecreaseVelocity();
+
+        }
+
+        //speed -= Mathf.PerlinNoise(Time.time, 0);
+
+        //Debug.Log(" look" + GamepadInput.GamePad.GetAxis(GamepadInput.GamePad.Axis.RightStick, GamepadInput.GamePad.Index.One).ToString());
+
+
+
+    }
+
+    private void DecreaseVelocity()
+    {
+        float xVelo = mRigidbody2d.velocity.x;
+        if(xVelo > 0)
+        {
+            xVelo -= Mathf.PerlinNoise(Time.time, 0);
+            xVelo = Mathf.Min(0, xVelo);
+        }
+        else
+        {
+            xVelo += Mathf.PerlinNoise(Time.time, 0);
+            xVelo = Mathf.Max(0, xVelo);
+        }
+
+        float yVelo = mRigidbody2d.velocity.y;
+        if (yVelo > 0)
+        {
+            yVelo -= Mathf.PerlinNoise(Time.time, 0);
+            yVelo = Mathf.Min(0, yVelo);
+        }
+        else
+        {
+            yVelo += Mathf.PerlinNoise(Time.time, 0);
+            yVelo = Mathf.Max(0, yVelo);
+        }
+
+        Vector2 newVelocity = new Vector2(xVelo,yVelo);
+       
+        mRigidbody2d.velocity = newVelocity;// Vector2.zero;
+
+    }
+
+
+    public void Damage(int i)
+    {
+        healthPoints -= i;
+        
+    }
+
+    private void InitVariables()
+    {
+        
+        if (mRigidbody2d == null)
+            mRigidbody2d = this.GetComponent<Rigidbody2D>();
+        if (mTransform == null)
+            mTransform = this.GetComponent<Transform>();
+    }
+
+
+
+
+
+}
