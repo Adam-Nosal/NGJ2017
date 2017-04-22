1a7b711e6931e9b8bd5a338591a9c9d578aacadb
Adam <nosal.adam11@gmail.com>
2017-04-23 01:38:13 +0200

    adding final UI - fixing artifacs problem

---
+++ trunk/Assets/Scripts/ClockHand.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockHand : MonoBehaviour {
+
+    [SerializeField] private RectTransform mTransform;
+
+    [SerializeField] private float rotationRangeMin;
+    [SerializeField] private float rotationRangeMax;
+    [SerializeField] private int ActivityMatter = 10;
+    private int activityIterator;
+    [SerializeField] private float ActivityForce = 25.0f;
+
+     void Awake()
+    {
+        mTransform = this.GetComponent<RectTransform>();
+    }
+
+
+     void Update()
+    {
+        activityIterator++;
+        if (activityIterator > ActivityMatter)
+        {
+            activityIterator = 0;
+            float actualRotation = mTransform.localRotation.eulerAngles.z;
+            float diff = Mathf.PerlinNoise(Time.time, 1) * ActivityForce;
+
+            if(Random.Range(0,10)>=5)
+                 actualRotation += diff;
+            else
+                actualRotation -= diff;
+
+            actualRotation = Mathf.Min(rotationRangeMin, actualRotation);
+            actualRotation = Mathf.Max(actualRotation, rotationRangeMax);
+
+            mTransform.rotation = Quaternion.Euler(0f, 0f, actualRotation);
+            
+        }
+    }
+
+}
---
+++ trunk/Assets/Scripts/SubmarineFlasher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubmarineFlasher : MonoBehaviour {
+
+    [SerializeField] private Image Submarine;
+    [SerializeField] private float frequencyPerSecond = 20;
+    private float timeOffset;
+    private float timeCounter;
+
+    private void Awake()
+    {
+        timeCounter = Time.time;
+        timeOffset = 1/ frequencyPerSecond;
+    }
+
+    private void Update()
+    {
+        if(Time.time > timeCounter + timeOffset)
+        {
+            Submarine.enabled = !Submarine.enabled;
+            timeCounter = Time.time;
+        }
+    }
+}
