9688325ef5249086f04a1d899cd7a15624a4c29e
Łukasz Biel <dotpoozer@gmail.com>
2017-04-22 15:27:34 +0200

    Knot is now singleton

--- trunk/Assets/Scripts/Knot.cs
+++ trunk/Assets/Scripts/Knot.cs
@@ -1,24 +1,7 @@
 ﻿using UnityEngine;
 
-public class Knot : MonoBehaviour
+public class Knot : Singleton<Knot>
 {
-    public static Knot instance { get; private set; }
-
     public int micIdx = 0;
-
-    public void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-        if(instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-
 }
 
