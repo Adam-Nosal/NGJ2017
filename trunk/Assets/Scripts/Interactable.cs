4e832673bbcc1057751d886cde279d63bb9c6285
Łukasz Biel <dotpoozer@gmail.com>
2017-04-22 02:58:19 +0200

    Removing render texture

---
+++ trunk/Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(EventSystem.current.IsPointerOverGameObject());
+        }
+	}
+}
