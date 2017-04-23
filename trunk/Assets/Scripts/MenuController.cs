419300d2b4ae41ec8288cc238ee272606e2e84a8
Łukasz Biel <dotpoozer@gmail.com>
2017-04-23 12:14:49 +0200

    audio

--- trunk/Assets/Scripts/MenuController.cs
+++ trunk/Assets/Scripts/MenuController.cs
@@ -7,6 +7,8 @@ public class MenuController : MonoBehaviour
 {
     public Dropdown microphoneMenu;
     public GameObject cthulu, fader;
+    public AudioSource src;
+    public AudioClip[] clips;
 
     private void Start()
     {
@@ -27,10 +29,19 @@ public class MenuController : MonoBehaviour
     private IEnumerator RunGameAsync()
     {
         fader.SetActive(true);
+        src.clip = clips[0];
+        src.Play();
         yield return new WaitForSeconds(0.5f);
         cthulu.SetActive(true);
-
-        yield return new WaitForSeconds(3f);
+        src.clip = clips[1];
+        src.Play();
+        yield return new WaitForSeconds(0.5f);
+        src.clip = clips[2];
+        src.Play();
+        yield return new WaitForSeconds(0.5f);
+        src.clip = clips[3];
+        src.Play();
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("main");
     }
 }
