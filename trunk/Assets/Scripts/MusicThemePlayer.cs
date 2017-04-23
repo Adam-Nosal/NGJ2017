0b5b60ec27445327619c36a042b38e82dab5d27e
Adam <nosal.adam11@gmail.com>
2017-04-23 09:21:44 +0200

    adding music theme player and fixing collision for one of the files

---
+++ trunk/Assets/Scripts/MusicThemePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicThemePlayer : MonoBehaviour {
+    private bool canPLay = true;
+
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioSource source;
+
+    private void Awake()
+    {
+        source.loop = false;
+        source.playOnAwake = false;
+    }
+    
+    private void PlaySound()
+    {
+        int index = Random.Range(0, audioClips.Length);
+        source.clip = audioClips[index];
+        source.Play();
+    }
+
+
+    private void Update()
+    {
+        canPLay = !source.isPlaying;
+
+
+        if (canPLay)
+        {
+            PlaySound();
+        }
+    }
+}
